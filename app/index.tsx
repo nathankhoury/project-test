c721cdf9b1e52f1c284d7ab59c43629585b31a6e
Nathan Khoury <nathankhoury0709@gmail.com>
2026-02-13 16:41:31 -0500

    add staff and note appearing on screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,50 @@
-import { Text, View } from "react-native";
+import { Image } from "expo-image"; // https://docs.expo.dev/versions/latest/sdk/image/#imagesource
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Test app to try drawing notes to the screen</Text>
+      <View style={styles.staffContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/BFMN_lib/Staves/Stave_lines_1_system_large.png")}
+        />
+        <Image
+          style={styles.note}
+          source={require("../assets/BFMN_lib/Notes/3_Quarter_Note_-_Crotchet/Stems_up/Quarter_Crotchet.png")}
+        />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    height: '100%',
+    width: '100%',
+    backgroundColor: '#0553',  // temp
+    resizeMode: 'contain',
+  },
+  staffContainer: {
+    height: '20%',
+    width: '100%',
+    
+    margin: 20
+  },
+  note: {
+    position: 'absolute',
+    width: '100%',
+    height: '50%',
+    left: 0,
+    top: 0,
+    resizeMode: 'contain',
+  }
+});

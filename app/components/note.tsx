8e5e01f1bb4b6851600115b5a49d846c65c63795
Nathan Khoury <nathankhoury0709@gmail.com>
2026-02-14 15:21:13 -0500

    add constant staff line heights to draw note on

--- app/components/note.tsx
+++ app/components/note.tsx
@@ -1,10 +1,35 @@
 import { Image } from "expo-image"; // https://docs.expo.dev/versions/latest/sdk/image/#imagesource
 import { StyleSheet } from "react-native";
 
-export default function Note() {
+/* 
+For now, all notes are quarter notes, and all are stem up.
+*/
+
+// define a new typescript type for the note props, this assures that the types are passed and
+// uses typescript's type checking to catch any errors 
+type NoteProps = {
+  name: string;
+  xPos: number; // this is the horizontal position of the note, used with xOffset to calculate the final position of the note
+  yPos: number; // this is the height of the line the note is on, used with yOffset to calculate the final position of the note
+  xOffset?: number; // ? means optional parameter, Note() constructor provides default value of 0
+  yOffset?: number;
+}
+
+export default function Note({
+  name,
+  xPos,
+  yPos,
+  xOffset = 0,
+  yOffset = 0
+}: NoteProps) {
+
+  const finalX = xPos - xOffset; // calculate the final x position of the note based on the x position and the x offset
+  const finalY = yPos - yOffset; // calculate the final y position of the note based on the line height and the y offset
+
   return (
+    // give all of the styles from the noteStyles sheet, under the "note" key. Override the top and left positions with the calculated finalY and finalX
     <Image
-        style={noteStyles.note}
+        style={[noteStyles.note, { top: finalY, left: finalX }]}
         source={require("../../assets/BFMN_lib/Notes/3_Quarter_Note_-_Crotchet/Stems_up/Quarter_Crotchet.png")}
     />
   );
@@ -12,11 +37,12 @@ export default function Note() {
 
 const noteStyles = StyleSheet.create({  
     note: {
-    position: 'absolute',
-    width: '100%',
-    height: '50%',
-    left: 0,
-    top: 0,
-    resizeMode: 'contain',
+      position: 'absolute',
+      width: 35,
+      height: 110,
+      zIndex: 10, // make sure the note is on top of the staff lines
+      left: 0,
+      top: 0,
+      resizeMode: 'contain',
   }
 });
--- app/index.tsx
+++ app/index.tsx
@@ -2,47 +2,86 @@ import { Image } from "expo-image"; // https://docs.expo.dev/versions/latest/sdk
 import { StyleSheet, Text, View } from "react-native";
 import Note from "./components/note";
 
-export default function Index() {
-  return (
-    <View
-      style={styles.container}
-    >
-      <Text>Test app to try drawing notes to the screen</Text>
-      <View style={styles.staffContainer}>
-        <Image
-          style={styles.image}
-          source={require("../assets/BFMN_lib/Staves/Stave_lines_1_system_large.png")}
-        />
-        <Note/>
-      </View>
-    </View>
-  );
-}
+  /*
+  START HERE:
+  I tried to leave many loose comments to keep the code organized for interpretation!
+  You can try changing the "lineHeight" constant as the parameter to the Note componentto move the note up and down on the staff.
+  
+  ----------------------------- line 5
+  ----------------------------- line 4
+  ----------------------------- line 3
+  ----------------------------- line 2
+  ----------------------------- line 1
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  image: {
-    height: '100%',
-    width: '100%',
-    backgroundColor: '#0553',  // temp
-    resizeMode: 'contain',
-  },
-  staffContainer: {
-    height: '20%',
-    width: '100%',
-    
-    margin: 20
-  },
-  note: {
-    position: 'absolute',
-    width: '100%',
-    height: '50%',
-    left: 0,
-    top: 0,
-    resizeMode: 'contain',
+  navigate from here (index.tsx) to ./components/note.tsx to see the note implementation.
+  Components in React Natives are the substitutes for classes in OOP languages.
+  By defining a Note component, we can create as many notes as we want by just calling the Note() function with different props (properties).
+  This works in this file by writing <Note> like a HTML element, and passing the props as attributes. The Note() function then takes 
+  those props and uses them to calculate the final position of the note on the staff, and returns an Image component with the note image at the correct position.
+
+
+
+  */
+
+  // I found the pixel height on the original image and divided by 2 since thats approx the ratio of the
+  // final displayed image to the original image.
+  // constant staff line heights:
+  const line1Height = 140;
+  const line2Height = 108;
+  const line3Height = 76;
+  const line4Height = 44;
+  const line5Height = 12;
+
+  // Currently the staff goes off the screen on mobile, so I use a const mobile x offset to keep the note on screen
+  const mobileXOffset = 180;
+
+  export default function Index() {
+    return (
+      <View
+        style={styles.container}
+      >
+        <Text>Test app to try drawing notes to the screen</Text>
+        <View style={styles.staffContainer}>
+          <Image
+            style={styles.image}
+            source={require("../assets/BFMN_lib/Staves/Stave_lines_1_system_large.png")}
+          />
+          <Note
+            name="Quarter Note"
+            xPos={mobileXOffset} // not yet useful, but constant value is used to keep note on screen
+            yPos={line5Height}  // TRY CHANGING TO OTHER CONSTANTS!
+            xOffset={17}  // should stay fixed for quarter notes!
+            yOffset={90}  // ^^^
+          />
+        </View>
+      </View>
+    );
   }
-});
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    image: {
+      height: '100%',
+      width: '100%',
+      backgroundColor: '#0553',  // temp
+      resizeMode: 'contain',
+    },
+    staffContainer: {
+      position: "relative",
+      height: 155,
+      width: 640,
+      margin: 20
+    },
+    note: {
+      position: 'absolute',
+      width: '100%',
+      height: '50%',
+      left: 0,
+      top: 0,
+      resizeMode: 'contain',
+    }
+  });
